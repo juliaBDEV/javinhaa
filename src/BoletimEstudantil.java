507a5d6794290f406acb9166e06af04328c53dff
JuliaDev <eu.hotmail5959@gmail.com>
2023-10-09 20:27:07 -0300

    alterações

--- src/BoletimEstudantil.java
+++ src/BoletimEstudantil.java
@@ -2,12 +2,12 @@ public class BoletimEstudantil {
     public static void main(String[] args) {
         
         int mediaFinal = 6;
-        if(mediaFinal<6)
-        System.out.println("REPROVADO");
-        else if(mediaFinal==6)
-        System,out.println("PROVA MINERVA");
-        else 
-        System.out.println("APROVADO");
+           if(mediaFinal<6)
+                System.out.println("REPROVADO");
+         else if(mediaFinal==6)
+             System.out.println("PROVA MINERVA");
+         else 
+         System.out.println("APROVADO");
     
     }
 }
