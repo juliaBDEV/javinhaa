044b21ebd4290f80e99b992dd1f3e049ca65d897
JuliaDev <eu.hotmail5959@gmail.com>
2023-10-09 21:34:37 -0300

    exemplos

--- src/edu/julia/primeirasemana/edu/julia/primeirasemana/TiposVariaveis.java
+++ src/edu/julia/primeirasemana/edu/julia/primeirasemana/TiposVariaveis.java
@@ -1,5 +1,16 @@
 package edu.julia.primeirasemana.edu.julia.primeirasemana;
 
 public class tiposVariaveis {
+   
+    public static void main(String[] args) {
+        
+        String meuNome = "JULIA BRANDÃO";
+
+        double salarioMinimo = 2500;
+            
+        
     
+      //final quer dizem pra nao mudar o valor 
+        final double VALOR DE PI = 3.14;
+    }
 }
