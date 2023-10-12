f6d43bcf5ab2fa5f5e085005ccff0c842e5200b0
JuliaDev <eu.hotmail5959@gmail.com>
2023-10-12 19:14:41 -0300

    classe nova

---
+++ src/edu/julia/Operadores.java
@@ -0,0 +1,27 @@
+package edu.julia;
+
+public class Operadores {
+
+   public static void main(String{}args)
+
+   {
+      int a, b;
+      a = 5;
+      b = 6;
+
+      String resultado = "";
+      if (a == b)
+         resultado = "verdadeiro";
+      else
+         resultado = "falso";
+
+      System.out.println(resultado);
+
+   }
+
+   @Override
+   public String toString() {
+      return "Operadores []";
+   }
+
+}
