e1c3e6d784b28b27c406e0938d49eca4e7d90e3a
JuliaDev <eu.hotmail5959@gmail.com>
2023-10-12 19:44:01 -0300

    novos dados

--- src/edu/julia/Operadores.java
+++ src/edu/julia/Operadores.java
@@ -4,7 +4,11 @@ public class Operadores {
 
    public static void main(String[] args){
 
-   
+     String nomeUm = "Julia";
+     String nomeDois = "Julia";
+     
+     System.out.println(nomeUm == nomeDois);
+
       int a, b;
       a = 5;
       b = 6;
