7619fa1a5fa5047a190d79582d7d92cde741fd18
JuliaDev <eu.hotmail5959@gmail.com>
2023-10-12 18:41:55 -0300

    confirmando

---
+++ src/edu/julia/TiposVariaveis.java
@@ -0,0 +1,15 @@
+package edu.julia;
+public class TiposVariaveis {
+   
+    public static void main(String[] args) {
+        
+        String meuNome = "JULIA BRANDÃO";
+
+        double salarioMinimo = 2500;
+            
+        
+    
+      //final quer dizem pra nao mudar o valor 
+        final double VALOR DE PI = 3.14;
+    }
+}
